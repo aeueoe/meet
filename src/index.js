6afa0e01b7c0d6f334d4fb9f45227459f557c202
aeueoe <drom.jul@gmail.com>
2024-08-14 17:23:01 +0200

    End-to-End Testing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import * as atatus from "atatus-spa";
+
+atatus.config("476cd617a1e04a3e8c612959c2d28213").install();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
